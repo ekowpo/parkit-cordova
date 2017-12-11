1fe395376a2ff200a46fb081c319681edc31f005
ekowpo <ekowpo@gmail.com>
2017-12-10 21:59:06 -0500

    Add project files.

---
+++ parkitV1/www/scripts/search.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(function () {
+    document.getElementById("username").innerHTML = localStorage.getItem("name");
+
+});
+
+    
+    function loadParkinglots() {
+        var url = "http://localhost:8080/api/parkinglot";
+
+        if (!$.fn.dataTable.isDataTable('#parkinglots')) {
+            var table = $('#parkinglots')
+                .DataTable(
+                {
+                    "order": [[1, "asc"]],
+                    "searching": false,
+                    "paging": false,
+                    ajax: {
+                        url: url,
+                        type: 'GET',
+                        draw: 1,
+                        data: {
+                            location: function () { return document.getElementById("pac-input").value; }
+                        }
+                    },
+                    columns: [
+                        {
+                            data: 'name'
+                        },
+                        {
+                            data: 'distance'
+                        },
+                        {
+                            data: 'mins'
+                        },
+                        {
+                            data: function (aData) {
+                                return '<a href="#!view?name="' + aData.name + '>View Price</a>';
+                            }
+                        }
+                    ],
+                    fnRowCallback: function (
+                        nRow, aData,
+                        iDisplayIndex,
+                        iDisplayIndexFull) {
+
+                        return nRow;
+                    }
+                });
+        }
+        else {
+            $('#parkinglots')
+                .DataTable().ajax.reload();
+        }
+    }
+
+
